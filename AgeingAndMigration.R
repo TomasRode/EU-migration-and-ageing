45217682893133f9e1c0d447a835328605144f71
TomasRode <tomi.rode@gmail.com>
2022-03-06 18:32:44 +0100

    Data and files first commit

---
+++ AgeingAndMigration.R
@@ -0,0 +1,5 @@
+################  IMPORT LIBRARIES #########################
+library(readr)
+
+################ IMPORT DATA ###############################
+migration_flows <- read.csv2("data/Migration_flows.csv")
